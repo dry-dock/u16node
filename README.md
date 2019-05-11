f8258b7d26da81871c33160e4e07f9e711f31430
ranjansingh41 <ranjan@shippable.com>
2019-05-11 11:19:49 +0000

    Adding node version v10.15.3 for u16node images

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # u16node
+
+Docker image with ubuntu 16.04 + nodejs
+
+Available versions are:
+
+1. 10.15.3
---
+++ test/_node.sh
@@ -0,0 +1,6 @@
+#! /bin/bash -e
+source ~/.nvm/nvm.sh
+
+echo "nvm use 10.15.3"
+nvm use 10.15.3
+printf "\n"
