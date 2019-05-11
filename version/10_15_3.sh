a59a1039be8b1d56f1c97bb792a6c577e2852042
ranjansingh41 <ranjan@shippable.com>
2019-05-11 11:26:34 +0000

    Adding node version v10.15.3 for u16node images

--- version/10_15_3.sh
+++ version/10_15_3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-NODE_VERSION=10.15.1
+NODE_VERSION=10.15.3
 NPM_VERSION=6.9.0
 echo "=============== Installing Node $NODE_VERSION ============="
 . /root/.nvm/nvm.sh && nvm install $NODE_VERSION
