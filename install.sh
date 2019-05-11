c04d1e24d2a670b9f03edb95dc461d731324c5b1
ranjansingh41 <ranjan@shippable.com>
2019-05-11 11:56:55 +0000

    Removing source env from u16node

--- install.sh
+++ install.sh
@@ -2,8 +2,7 @@
 
 echo "================= Installing basic packages ==================="
 apt-get update
-apt-get install -y \
-curl \
+apt-get install -y curl
 
 export NVM_VERSION=v0.33.9
 echo "================= Installing NVM "$NVM_VERSION" ==================="
@@ -12,10 +11,6 @@ curl -sS https://raw.githubusercontent.com/creationix/nvm/"$NVM_VERSION"/install
 # Set NVM_DIR so the installations go to the right place
 export NVM_DIR="/root/.nvm"
 
-# add source of nvm to .bashrc - allows to use nvm as a command
-echo "source ~/.nvm/nvm.sh"
-
-for file in /u16node/version/*;
-do
+for file in /u16node/version/*; do
   $file
 done
