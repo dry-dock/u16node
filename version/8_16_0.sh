2c7ec22f3c8c3fbe14dddb2ced3728575c26a31a
ranjansingh41 <ranjan@shippable.com>
2019-05-13 14:12:38 +0000

    Adding clis and nodejs8.16.0 for u16node images

--- version/8_16_0.sh
+++ version/8_16_0.sh
@@ -1,8 +1,7 @@
 #!/bin/bash -e
 
-NODE_VERSION=10.15.3
+NODE_VERSION=8.16.0
 NPM_VERSION=6.9.0
 echo "=============== Installing Node $NODE_VERSION ============="
 . /root/.nvm/nvm.sh && nvm install $NODE_VERSION
 npm install npm@$NPM_VERSION -g
-nvm alias default $NODE_VERSION
