6520ec01446eecd06b1f38f2436f698b7a8d5d74
ranjansingh41 <ranjan@shippable.com>
2019-05-15 07:32:10 +0000

    Adding jq for u16node image

--- install.sh
+++ install.sh
@@ -61,7 +61,7 @@ echo "================ Adding azure-cli $AZURE_CLI_VERSION =============="
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ wheezy main" | \
 sudo tee /etc/apt/sources.list.d/azure-cli.list
 curl -L https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
-sudo apt-get install -q apt-transport-https
+sudo apt-get install -q apt-transport-https=1.2*
 sudo apt-get update && sudo apt-get install -y -q azure-cli=$AZURE_CLI_VERSION
 
 JFROG_VERSION=1.25.0
